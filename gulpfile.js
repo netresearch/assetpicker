6ad0ff091614630bccb29e272badc24b00cc1fdf
Christian Opitz <christian.opitz@netresearch.de>
2016-10-20 17:47:04 +0200

    Added release task for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -60,6 +60,113 @@ gulp.task('start-server', function() {
     connect.server({ root: 'dist', livereload: true });
 });
 
+gulp.task('release', function (cb) {
+    const readline = require('readline'),
+          cp = require('child_process'),
+          fs = require('fs');
+    var ask = function (question, options, callback) {
+            question += ' ';
+            if (typeof options === 'function') {
+                callback = options;
+                options = undefined;
+            } else {
+                question += '[' + options.join(',') + '] ';
+            }
+            var rl = readline.createInterface({
+                    input: process.stdin,
+                    output: process.stdout
+                }),
+                handler = function(answer) {
+                    if (options && options.indexOf(answer) === -1) {
+                        console.log('Invalid answer - please type ' + options.slice(0, -1).join(', ') + ' or ' + options.slice(0).pop());
+                        rl.question(question, handler);
+                        return;
+                    }
+                    callback(answer);
+                    rl.close();
+                };
+            rl.question(question, handler);
+        },
+        git = function (command, successCallback, errorCallback) {
+            cp.exec('git ' + command, function(err, stdout, stderr) {
+                if (err) {
+                    if (errorCallback) {
+                        errorCallback(stderr);
+                    } else {
+                        console.error(stderr);
+                        cb('git ' + command + ' failed');
+                    }
+                } else if (successCallback) {
+                    successCallback(stdout.replace(/\s+$/, ''));
+                }
+            });
+        };
+    git ('status -s', function (changes) {
+        var proceed = function () {
+            git('tag -l', function (tags) {
+                var tag = tags.split("\n").pop();
+                console.log('The latest tag is ' + tag + ' - choose which should be the next:');
+                var parts = tag.split('.'), nextTags = {};
+                for (var i = parts.length - 1; i >= 0; i--) {
+                    var currentParts = parts.slice(0), n = parts.length - i;
+                    for (j = parts.length - 1; j > i; j--) {
+                        currentParts[j] = 0;
+                    }
+                    currentParts[i] = parseInt(parts[i]) + 1;
+                    nextTags[n] = currentParts.join('.');
+                    console.log(n + ') ' + nextTags[n]);
+                }
+                ask('Next version?', Object.keys(nextTags), function (nextTag) {
+                    nextTag = nextTags[nextTag];
+                    cp.exec('npm version --no-git-tag-version ' + nextTag, function (err, stdout, stderr) {
+                        if (err) {
+                            if (stderr.toString().indexOf('Version not changed') === -1) {
+                                throw stderr.toString();
+                            }
+                        }
+                        cp.exec('npm publish', function (err, stdout, stderr) {
+                            if (err) {
+                                if (stderr.toString().match(/code\s+ENEEDAUTH/)) {
+                                    cb('Not authenticated to npm - run npm adduser');
+                                    return;
+                                } else {
+                                    throw stderr.toString();
+                                }
+                            }
+                            fs.writeFileSync(
+                                'README.md',
+                                fs.readFileSync('README.md').toString().replace(new RegExp(tag.replace(/\./, '\\.'), 'g'), nextTag)
+                            );
+                            git('commit -m "Bumped version to' + nextTag + '" package.json README.md', function () {
+                                git('log ' + tag + '..HEAD --format="- %s"', function (log) {
+                                    fs.writeFileSync('.commit-msg', 'Tagging ' + nextTag + ":\n" + log);
+                                    git('tag -a ' + nextTag + ' -F .commit-msg', function () {
+                                        fs.unlinkSync('.commit-msg');
+                                        git('push --follow-tags', function () {
+                                            cb();
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        };
+        if (changes) {
+            ask("You have uncommited changes:\n" + changes + "\nDo you want to proceed without commiting them?", ['y', 'n'], function (answer) {
+                if (answer === 'y') {
+                    proceed();
+                } else {
+                    cb();
+                }
+            });
+        } else {
+            proceed();
+        }
+    });
+});
+
 gulp.task('compile', ['html', 'sass', 'js']);
 gulp.task('watch', function () {
     livereload.listen();
